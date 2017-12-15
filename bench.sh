2b65d3a4b782ec0fdf5d27f2011230e49c1e8690
Christopher Yeh <chrisyeh96@gmail.com>
2017-12-15 01:11:12 -0500

    Update shell script to include go

--- bench.sh
+++ bench.sh
@@ -7,11 +7,13 @@ make clean
 make
 
 # producer bound
+echo "pc-pthreads: producer-bound"
 time ./pc-pthreads -e 100 -i
 time ./pc-pthreads -e 1000 -i
 time ./pc-pthreads -e 10000 -i
 
 # consumer bound
+echo "pc-pthreads: consumer-bound"
 time ./pc-pthreads -e 100 -j
 time ./pc-pthreads -e 1000 -j
 time ./pc-pthreads -e 10000 -j
@@ -28,11 +30,13 @@ make clean
 make
 
 # producer bound
+echo "pc-libmill: producer-bound"
 time ./pc-libmill -e 100 -i
 time ./pc-libmill -e 1000 -i
 time ./pc-libmill -e 10000 -i
 
 # consumer bound
+echo "pc-libmill: consumer-bound"
 time ./pc-libmill -e 100 -j
 time ./pc-libmill -e 1000 -j
 time ./pc-libmill -e 10000 -j
@@ -49,11 +53,13 @@ cargo clean
 cargo build --release
 
 # producer bound
+echo "pc-rust: producer-bound"
 time ./target/release/pc-rust -e 100 -i
 time ./target/release/pc-rust -e 1000 -i
 time ./target/release/pc-rust -e 10000 -i
 
 # consumer bound
+echo "pc-rust: consumer-bound"
 time ./target/release/pc-rust -e 100 -j
 time ./target/release/pc-rust -e 1000 -j
 time ./target/release/pc-rust -e 10000 -j
@@ -61,3 +67,25 @@ time ./target/release/pc-rust -e 10000 -j
 # go back to root dir
 cd ../
 
+####
+# go
+####
+# build
+cd ./pc-go/
+rm producer-consumer
+go build producer-consumer.go
+
+# producer bound
+echo "pc-go: producer-bound"
+time ./producer-consumer -e 100 -i
+time ./producer-consumer -e 1000 -i
+time ./producer-consumer -e 10000 -i
+
+# consumer bound
+echo "pc-go: consumer-bound"
+time ./producer-consumer -e 100 -j
+time ./producer-consumer -e 1000 -j
+time ./producer-consumer -e 10000 -j
+
+# go back to root dir
+cd ../
